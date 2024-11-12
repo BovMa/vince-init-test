ab052b5a90deb5a758b5f81ccded27f155a5a2c0
BovMa <git@stackblitz.com>
2024-11-13 00:51:13 +0100

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
+
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['icon.svg'],
+      manifest: {
+        name: 'HumanitarianJobs',
+        short_name: 'HumJobs',
+        description: 'Find meaningful humanitarian job opportunities worldwide',
+        theme_color: '#1d4ed8',
+        icons: [
+          {
+            src: 'icon-192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'icon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
+});
